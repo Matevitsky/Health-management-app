b68626582c8cb2bf4e57243ac95002ce6d3d2e75
Sergey <sergey.m23@gmail.com>
2016-08-12 17:58:58 +0300

    HW9_binding_ajax

--- src/main/java/ru/javawebinar/topjava/web/meal/UserMealAjaxController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/UserMealAjaxController.java
@@ -1,15 +1,15 @@
 package ru.javawebinar.topjava.web.meal;
 
-import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.javawebinar.topjava.model.UserMeal;
-import ru.javawebinar.topjava.to.UserMealTo;
 import ru.javawebinar.topjava.to.UserMealWithExceed;
-import ru.javawebinar.topjava.util.UserMealsUtil;
 
+import javax.validation.Valid;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
 
@@ -17,6 +17,17 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/ajax/profile/meals")
 public class UserMealAjaxController extends AbstractUserMealController {
+
+    /*
+   Other validation solution:
+   http://docs.spring.io/spring/docs/current/spring-framework-reference/html/mvc.html#mvc-ann-initbinder
+
+     @InitBinder
+     protected void initBinder(WebDataBinder binder) {
+         binder.setRequiredFields("dateTime", "description", "calories");
+     }
+ */
+
 
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public List<UserMealWithExceed> getAll() {
@@ -31,26 +42,23 @@ public class UserMealAjaxController extends AbstractUserMealController {
 
 
     @RequestMapping(method = RequestMethod.POST)
-    public void updateOrCreate(@RequestParam("id") Integer id,
-                                                 @RequestParam("dateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateTime,
-                                                  @RequestParam("description") String description,
-                                                 @RequestParam("calories") int calories) {
+    public ResponseEntity<String> updateOrCreate(@Valid UserMeal meal, BindingResult result) {
 
-        /*if (result.hasErrors()) {
+        if (result.hasErrors()) {
             StringBuilder sb = new StringBuilder();
             result.getFieldErrors().forEach(fe -> sb.append(fe.getField()).append(" ").append(fe.getDefaultMessage()).append("<br>"));
             return new ResponseEntity<>(sb.toString(), HttpStatus.UNPROCESSABLE_ENTITY);
-        }*/
-        UserMealTo userMealTo = new UserMealTo(id, dateTime, description, calories);
+        }
 
-        if (userMealTo.isNew()) {
-            super.create(UserMealsUtil.updateFromTo(new UserMeal(), userMealTo));
+        if (meal.isNew()) {
+            super.create(meal);
         } else {
 
-            super.update(userMealTo, id);
+            super.update(meal, meal.getId());
         }
-        //return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
+
 
     @RequestMapping(value = "/filter", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     public List<UserMealWithExceed> getBetween(
@@ -59,7 +67,7 @@ public class UserMealAjaxController extends AbstractUserMealController {
         return super.getBetween(startDate, startTime, endDate, endTime);
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/{id}")
     public UserMeal get(@PathVariable("id") int id) {
         return super.get(id);
     }
